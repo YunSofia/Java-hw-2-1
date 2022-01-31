1615abc097c4390d9902c67afa8a89570a2d8480
Yun Sofia <sonyayun@yandex.ru>
2022-01-31 11:21:20 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ticketPrice = 100;
+        int priceMile = 20;
+        int totalMile = ticketPrice / priceMile;
+        System.out.println(totalMile + " бонусные мили.");
+    }
+}
